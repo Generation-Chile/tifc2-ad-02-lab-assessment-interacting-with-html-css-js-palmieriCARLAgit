5bfc9571e02f433d02a49d268aae03103963bde1
palmieriCARLAgit <carla.palmieri@genstudents.org>
2024-04-26 22:24:13 -0400

    listoooo

--- script2.js
+++ script2.js
@@ -1,12 +1,17 @@
 document.getElementById("titulo").innerHTML="Adiós"
 document.getElementById("titulo").style.color="orange"
 
-
-    // en proceso
 function cambiocolor(){
     document.body.style.backgroundColor="pink"
 }
 
 function cambiocolor2(){
     document.getElementById("rojo").style.color="brown"
+}
+
+function cambiotexto(){
+    document.getElementById("titulo").innerHTML="cambiando el texto desde una funcion de JS, amo<3"
+}
+function colorfondo(){
+    document.body.style.backgroundColor="pink"
 }

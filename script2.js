bb0712cefe0915d84051bc89ceba98ac85771a17
palmieriCARLAgit <carla.palmieri@genstudents.org>
2024-04-26 21:50:19 -0400

    en proceso

---
+++ script2.js
@@ -0,0 +1,9 @@
+document.getElementById("titulo").innerHTML="Adiós"
+document.getElementById("titulo").style.color="orange"
+
+
+    // en proceso
+clikea(){
+    const roojo= document.getElementById("rojo").style.color="brown"
+    onclick
+}
